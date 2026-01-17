345595f94bd975f69e8e8a6f34aef42b670b6dea
pranavgawande10 <pranavgawande05@gmail.com>
2026-01-18 00:36:00 +0530

    Setup Redux Store

--- README.md
+++ README.md
@@ -3,10 +3,21 @@
 -Create vite+react 
 -remove unecessary code and create hello world app
 -insatll tailwind css
--install Daisy UI 
+-install Daisy UI   
 -add nav bar component to App.jsx
 -create a NavBarjsx seprate component file
 -install ract router dom
 - Create BrowserRouter > Routes > Route=/Body > Route Children 
 -Create an outlet in an body component
 - Create Footer
+- Create Login Page
+- Install axios
+-Cors- Install Cors in backend => add middleware to app.js and add configurations: origin :"localhost URL of frontend", credentials :true;
+-If you are making API call to pass the axios => {withCredentials : true}
+- install react-redux  + @reduxjs/toolkit (https://redux-toolkit.js.org/tutorials/quick-start)
+-  => configureStore => Provider => create Slice => app reducer to store
+-Add redux devtools in chrome 
+- login and see  if your data is coming properly in store 
+- NavBar should update as soon as user login
+- Refactor our code to add constants file + create component folder 
+ 
---
+++ src/components/Login.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("jadhav@gmail.com");
+  const [password, setPassword] = useState("Aryan@1234");
+     const navigate  = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogin = async () => {
+   
+
+    try {
+      const res = await axios.post(
+        BASE_URL+ "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      // ✅ Store user data in Redux
+      dispatch(addUser(res.data));
+      return navigate("/");
+
+    } catch (err) {
+      console.error("Login failed:", err.response?.data || err.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login!</h2>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email ID</legend>
+            <input
+              type="text"
+              value={emailId}
+              className="input"
+              placeholder="Type here"
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              value={password}
+              className="input"
+              placeholder="Type here"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
+
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
